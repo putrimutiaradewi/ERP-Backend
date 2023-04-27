a1be8a42db60e9bfbc116f8fc5163b4020f4ed93
dzaky gea <dzakygea1@gmail.com>
2023-04-27 09:01:14 +0700

    first upload

--- src/path.rs
+++ src/path.rs
@@ -1,13 +1,10 @@
 use sqlx::{Pool,Postgres};
-use be_erp::{table_list, add_table, delete_table, update_table, add_account, update_account, login_account};
+use be_erp::{ update_account, login_account, tambah_akun};
 use tide::{Server};
 
 pub fn path(app: &mut Server<Pool<Postgres>>){
-app.at("cek").get(table_list);
-app.at("add").post(add_table);
-app.at("update").put(update_table);
-app.at("delete").delete(delete_table);
-app.at("Register").post(add_account);
+
 app.at("update").put(update_account);
 app.at("login").post(login_account);
+app.at("daftar").post(tambah_akun);
 }
